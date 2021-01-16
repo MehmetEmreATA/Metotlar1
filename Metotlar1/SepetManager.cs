6f0b766a3707d9efef0673393a69b7811aacbb18
Mehmet Emre ATA <by.karakule.2001@gmail.com>
2021-01-16 23:50:57 +0300

    Add project files.

---
+++ Metotlar1/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar1
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat)
+        {
+            Console.WriteLine("Tebriler. Sepete eklendi :" + urunAdi);
+        }
+    }
+}
